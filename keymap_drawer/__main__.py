e85fbd05836631af24db873905c97cf2191f0991
Cem Aksoylar <caksoylar@users.noreply.github.com>
2022-09-12 23:47:46 -0700

    Get physical layout from QMK info.json if provided

--- keymap_drawer/__main__.py
+++ keymap_drawer/__main__.py
@@ -3,6 +3,7 @@ Script that takes a yaml containing physical keyboard layout and
 keymap descriptions, then prints an SVG representing the keymap.
 """
 import sys
+import json
 from ruamel import yaml
 
 from .draw import KeymapDrawer
@@ -12,7 +13,14 @@ def main() -> None:
     """Parse the configuration and print SVG using KeymapDrawer."""
     with open(sys.argv[1], "rb") as f:
         data = yaml.safe_load(f)
-    drawer = KeymapDrawer(**data)
+    layers = data["layers"]
+    if len(sys.argv) > 2:
+        with open(sys.argv[2], 'rb') as f:
+            qmk_json = json.load(f)
+        layout = next(iter(qmk_json["layouts"].values()))["layout"]  # take the first layout in map
+        drawer = KeymapDrawer(layers=data["layers"], layout={"ltype": "qmk", "layout": layout})
+    else:
+        drawer = KeymapDrawer(layers=layers, layout=data["layout"])
     drawer.print_board()
 
 
--- keymap_drawer/physical_layout.py
+++ keymap_drawer/physical_layout.py
@@ -3,11 +3,10 @@ Module containing classes pertaining to the physical layout of a keyboard,
 i.e. a sequence of keys each represented by its coordinates, dimensions
 and rotation.
 """
-from abc import ABC
 from functools import cached_property
-from typing import Sequence, ClassVar, Literal
+from typing import Sequence, Literal
 
-from pydantic import BaseModel, validator, root_validator
+from pydantic import BaseModel, root_validator
 
 KEY_W = 59
 KEY_H = 54
@@ -27,11 +26,10 @@ class PhysicalKey(BaseModel):
 LayoutType = Literal["ortho", "raw"]
 
 
-class PhysicalLayout(BaseModel, ABC):
+class PhysicalLayout(BaseModel):
     """Represents the physical layout of keys on the keyboard, as a sequence of keys."""
 
     keys: Sequence[PhysicalKey]
-    ltype: ClassVar[LayoutType]
 
     class Config:  # pylint: disable=missing-class-docstring
         arbitrary_types_allowed = True
@@ -55,39 +53,28 @@ def layout_factory(ltype: LayoutType, **kwargs) -> PhysicalLayout:
     """Create and return a physical layout as determined by the ltype argument."""
     match ltype:
         case "ortho":
-            return OrthoLayout(**kwargs)
+            return PhysicalLayout(keys=OrthoGenerator(**kwargs).generate())
+        case "qmk":
+            return PhysicalLayout(keys=QmkGenerator(**kwargs).generate())
         case "raw":
-            return RawLayout(**kwargs)
+            return PhysicalLayout(**kwargs)
         case _:
             raise ValueError(f'Physical layout type "{ltype}" is not supported')
 
 
-class RawLayout(PhysicalLayout):
+class OrthoGenerator(BaseModel):
     """
-    Physical layout directly given by a sequence of maps containing center coordinates,
-    width, height and optionally rotation of each key.
-    """
-
-    ltype: ClassVar[LayoutType] = "raw"
-
-    @validator("keys", pre=True, each_item=True, check_fields=False)
-    def parse_keys(cls, val):
-        """Parse each item containing a mapping into a PhysicalKey object."""
-        return PhysicalKey(**val)
-
-
-class OrthoLayout(PhysicalLayout):
-    """
-    Physical layout representing an ortholinear keyboard, as specified by its number of
-    rows, columns, thumb keys and whether it is split or not. If split, row/columnn number
-    represents the size of one half. Thumb keys can only be used if split.
+    Generator for a physical layout representing an ortholinear keyboard, as specified by
+    its number of rows, columns, thumb keys and whether it is split or not. If split,
+    row/column number represents the size of one half. Thumb keys can only be used if split.
     """
 
     split: bool
     rows: int
     columns: int
     thumbs: int | Literal["MIT", "2x2u"] = 0
-    ltype: ClassVar[LayoutType] = "ortho"
+    drop_pinky: bool = False
+    drop_inner: bool = False
 
     @root_validator
     def check_thumbs(cls, vals):
@@ -112,47 +99,94 @@ class OrthoLayout(PhysicalLayout):
                 ), "Cannot use MIT or 2x2u bottom row layout with odd number of columns"
         return vals
 
-    @root_validator(pre=True, skip_on_failure=True)
-    def create_ortho_layout(cls, vals):
-        """Create a list of PhysicalKeys from given ortho specifications."""
-        nrows = vals["rows"]
-        ncols = vals["columns"]
-        thumbs = vals.get("thumbs", 0)
-        keys = []
-        vals["keys"] = keys
+    @root_validator
+    def check_drops(cls, vals):
+        """Check that drop_pinky or drop_index are only used with split layouts."""
+        if vals["drop_pinky"] or vals["drop_inner"]:
+            assert vals["split"], '"drop_*" properties can only be used with split layouts'
+        return vals
 
-        def create_row(x: float, y: float, ncols: int = ncols) -> None:
+    def generate(self) -> Sequence[PhysicalKey]:
+        """Generate a list of PhysicalKeys from given ortho specifications."""
+        nrows = self.rows
+        if not isinstance(self.thumbs, int):
+            nrows -= 1
+        ncols = self.columns
+
+        keys: list[PhysicalKey] = []
+
+        def create_row(x: float, y: float, ncols: int = ncols) -> list[PhysicalKey]:
+            row_keys = []
             for _ in range(ncols):
-                keys.append(PhysicalKey(x_pos=x + KEY_W / 2, y_pos=y + KEY_H / 2))
+                row_keys.append(PhysicalKey(x_pos=x + KEY_W / 2, y_pos=y + KEY_H / 2))
                 x += KEY_W
+            return row_keys
 
         x, y = 0.0, 0.0
-        for _ in range(nrows if isinstance(thumbs, int) else nrows - 1):
-            create_row(x, y)
-            if vals["split"]:
-                create_row(x + ncols * KEY_W + SPLIT_GAP, y)
+        for row in range(nrows):
+            row_keys = create_row(x, y)
+            if self.split:
+                row_keys += create_row(x + ncols * KEY_W + SPLIT_GAP, y)
+
+            drop_cols = [0, -1] * self.drop_pinky + [
+                len(row_keys) // 2 - 1,
+                len(row_keys) // 2,
+            ] * self.drop_inner
+            for col in reversed(drop_cols):
+                if row < nrows - 1:
+                    row_keys[col].y_pos += KEY_H / 2
+                else:
+                    row_keys.pop(col)
+
+            keys.extend(row_keys)
             y += KEY_H
 
-        if not thumbs:
-            return vals
+        if not self.thumbs:
+            return keys
 
-        if isinstance(thumbs, int):  # implies split
-            create_row((ncols - thumbs) * KEY_W, y, thumbs)
-            create_row(ncols * KEY_W + SPLIT_GAP, y, thumbs)
-        elif thumbs == "MIT":
-            create_row(0.0, y, ncols // 2 - 1)
+        if isinstance(self.thumbs, int):  # implies split
+            keys += create_row((ncols - self.thumbs) * KEY_W, y, self.thumbs)
+            keys += create_row(ncols * KEY_W + SPLIT_GAP, y, self.thumbs)
+        elif self.thumbs == "MIT":
+            keys += create_row(0.0, y, ncols // 2 - 1)
             keys.append(
                 PhysicalKey(x_pos=(ncols / 2) * KEY_W, y_pos=y + KEY_H / 2, width=2 * KEY_W)
             )
-            create_row((ncols / 2 + 1) * KEY_W, y, ncols // 2 - 1)
+            keys += create_row((ncols / 2 + 1) * KEY_W, y, ncols // 2 - 1)
         else:  # "2x2u"
-            create_row(0.0, y, ncols // 2 - 2)
+            keys += create_row(0.0, y, ncols // 2 - 2)
             keys.append(
                 PhysicalKey(x_pos=(ncols / 2 - 1) * KEY_W, y_pos=y + KEY_H / 2, width=2 * KEY_W)
             )
             keys.append(
                 PhysicalKey(x_pos=(ncols / 2 + 1) * KEY_W, y_pos=y + KEY_H / 2, width=2 * KEY_W)
             )
-            create_row((ncols / 2 + 2) * KEY_W, y, ncols // 2 - 2)
+            keys += create_row((ncols / 2 + 2) * KEY_W, y, ncols // 2 - 2)
 
-        return vals
+        return keys
+
+
+class QmkGenerator(BaseModel):
+    """Generator for layouts given by QMK's info.json format."""
+
+    class QmkKey(BaseModel):
+        """Model representing each key in QMK's layout definition."""
+
+        x: float
+        y: float
+        w: float = 1.0
+        h: float = 1.0
+
+    layout: Sequence[QmkKey]
+
+    def generate(self) -> Sequence[PhysicalKey]:
+        """Generate a sequence of PhysicalKeys from QmkKeys."""
+        return [
+            PhysicalKey(
+                x_pos=KEY_H * (k.x + k.w / 2),
+                y_pos=KEY_H * (k.y + k.h / 2),
+                width=KEY_H * k.w,
+                height=KEY_H * k.h,
+            )
+            for k in self.layout
+        ]
