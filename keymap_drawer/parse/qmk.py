caksoylar/keymap-drawer

commit a91689cd6a02abb239cc923273cdebc70329b3c0

feat: Add config for removing keycode prefixes
Make configurable a set of prefixes to remove from keycodes before
further processing. For QMK we already removed "KC_", now you can add
prefixes for locale header files.

Similarly, you can add such prefixes if you use locale headers generated
by https://github.com/joelspadin/zmk-locale-generator/, but make sure to
guard the #include of the header so keymap-drawer doesn't expand the keycodes
first:

    #ifndef KEYMAP_DRAWER
    #include "keys_de.h"
    #endif

keymap_drawer/parse/qmk.py
@@ -4,6 +4,7 @@ import json
 import re
 from typing import Sequence
 
+from keymap_drawer.config import ParseConfig
 from keymap_drawer.keymap import KeymapData, LayoutKey
 from keymap_drawer.parse.parse import KeymapParser
 
@@ -11,7 +12,6 @@ from keymap_drawer.parse.parse import KeymapParser
 class QmkJsonParser(KeymapParser):
     """Parser for json-format QMK keymaps, like Configurator exports or `qmk c2json` outputs."""
 
-    _prefix_re = re.compile(r"\bKC_")
     _trans_re = re.compile(r"TRANSPARENT|TRNS|_______")
     _mo_re = re.compile(r"MO\((\d+)\)")
     _tog_re = re.compile(r"(TG|TO|DF)\((\d+)\)")
@@ -20,6 +20,20 @@ class QmkJsonParser(KeymapParser):
     _lt_re = re.compile(r"LT\((\d+), *(\S+)\)")
     _osm_re = re.compile(r"OSM\(MOD_(\S+)\)")
     _osl_re = re.compile(r"OSL\((\d+)\)")
+
+    def __init__(
+        self,
+        config: ParseConfig,
+        columns: int | None,
+        base_keymap: KeymapData | None = None,
+        layer_names: list[str] | None = None,
+    ):
+        super().__init__(config, columns, base_keymap, layer_names)
+        self._prefix_re: re.Pattern | None
+        if prefixes := self.cfg.qmk_remove_keycode_prefix:
+            self._prefix_re = re.compile(r"\b(" + "|".join(re.escape(prefix) for prefix in set(prefixes)) + ")")
+        else:
+            self._prefix_re = None
 
     def _str_to_key(  # pylint: disable=too-many-return-statements
         self, key_str: str, current_layer: int, key_positions: Sequence[int]
@@ -32,9 +46,9 @@ class QmkJsonParser(KeymapParser):
         assert self.layer_names is not None
 
         def mapped(key: str) -> LayoutKey:
+            if self._prefix_re is not None:
+                key = self._prefix_re.sub("", key)
             return LayoutKey.from_key_spec(self.cfg.qmk_keycode_map.get(key, key.replace("_", " ")))
-
-        key_str = self._prefix_re.sub("", key_str)
 
         if m := self._trans_re.fullmatch(key_str):  # transparent
             return self.trans_key

keymap_drawer/config.py
@@ -242,7 +242,11 @@ class ParseConfig(BaseSettings, env_prefix="KEYMAP_", extra="ignore"):
     # layer is active (which is the default behavior) or *any* of them (with this option)
     mark_alternate_layer_activators: bool = False
 
-    # convert QMK keycodes to their display forms, omitting "KC_" prefix on the keys
+    # remove these prefixes from QMK keycodes before further processing
+    # can be augmented with other locale prefixes, e.g. "DE_"
+    qmk_remove_keycode_prefix: list[str] = ["KC_"]
+
+    # convert QMK keycodes to their display forms, after removing prefixes in `qmk_remove_keycode_prefix`
     qmk_keycode_map: dict[str, str | dict] = {
         # QMK keycodes
         "XXXXXXX": "",
@@ -312,6 +316,10 @@ class ParseConfig(BaseSettings, env_prefix="KEYMAP_", extra="ignore"):
         "QUESTION": "?",
         "QUES": "?",
     }
+
+    # remove these prefixes from ZMK keycodes before further processing
+    # can be augmented with locale prefixes for zmk-locale-generator headers, e.g. "DE_"
+    zmk_remove_keycode_prefix: list[str] = []
 
     # convert ZMK keycodes to their display forms, applied to parameters of behaviors like "&kp"
     zmk_keycode_map: dict[str, str | dict] = {

keymap_drawer/parse/zmk.py
@@ -31,6 +31,11 @@ class ZmkKeymapParser(KeymapParser):
         self.hold_taps = {"&mt": ["&kp", "&kp"], "&lt": ["&mo", "&kp"]}
         self.mod_morphs = {"&gresc": ["&kp ESC", "&kp GRAVE"]}
         self.sticky_keys = {"&sk": ["&kp"], "&sl": ["&mo"]}
+        self._prefix_re: re.Pattern | None
+        if prefixes := self.cfg.zmk_remove_keycode_prefix:
+            self._prefix_re = re.compile(r"\b(" + "|".join(re.escape(prefix) for prefix in set(prefixes)) + ")")
+        else:
+            self._prefix_re = None
 
     def _update_raw_binding_map(self, dts: DeviceTree) -> None:
         raw_keys = list(self.raw_binding_map.keys())
@@ -54,6 +59,8 @@ class ZmkKeymapParser(KeymapParser):
         assert self.layer_names is not None
 
         def mapped(key: str) -> LayoutKey:
+            if self._prefix_re is not None:
+                key = self._prefix_re.sub("", key)
             mapped = LayoutKey.from_key_spec(
                 self.cfg.zmk_keycode_map.get(
                     key,

commit 698007928d62f29ff328a48cd40c27e827825158

fix: Fix transparent key detection broken by a91689c
Fixes #62

keymap_drawer/parse/qmk.py
@@ -12,7 +12,7 @@ from keymap_drawer.parse.parse import KeymapParser
 class QmkJsonParser(KeymapParser):
     """Parser for json-format QMK keymaps, like Configurator exports or `qmk c2json` outputs."""
 
-    _trans_re = re.compile(r"TRANSPARENT|TRNS|_______")
+    _trans_re = re.compile(r"KC_TRANSPARENT|KC_TRNS|_______")
     _mo_re = re.compile(r"MO\((\d+)\)")
     _tog_re = re.compile(r"(TG|TO|DF)\((\d+)\)")
     _mts_re = re.compile(r"([A-Z_]+)_T\((\S+)\)")

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "keymap-drawer"
-version = "0.13.0"
+version = "0.13.1"
 description = "A module and CLI tool to help parse and draw keyboard layouts."
 authors = ["Cem Aksoylar <caksoylar@gmail.com>"]
 readme = ["README.md", "KEYMAP_SPEC.md"]