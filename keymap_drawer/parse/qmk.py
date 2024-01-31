caksoylar/keymap-drawer

commit e9efae1948c216a9e64d817d8289cbf7fe735a23

feat: Add error reporting for individual key parsing

keymap_drawer/parse/qmk.py
@@ -6,7 +6,7 @@ from typing import Sequence
 
 from keymap_drawer.config import ParseConfig
 from keymap_drawer.keymap import KeymapData, LayoutKey
-from keymap_drawer.parse.parse import KeymapParser
+from keymap_drawer.parse.parse import KeymapParser, ParseError
 
 
 class QmkJsonParser(KeymapParser):
@@ -103,10 +103,17 @@ class QmkJsonParser(KeymapParser):
                 f"Length of provided layer name list ({l_u}) does not match the number of parsed layers ({num_layers})"
             )
 
-        layers = {
-            self.layer_names[ind]: [self._str_to_key(key, ind, [i]) for i, key in enumerate(layer)]
-            for ind, layer in enumerate(raw["layers"])
-        }
+        layers: dict[str, list[LayoutKey]] = {}
+        for layer_ind, layer in enumerate(raw["layers"]):
+            layer_name = self.layer_names[layer_ind]
+            layers[layer_name] = []
+            for ind, key in enumerate(layer):
+                try:
+                    layers[layer_name].append(self._str_to_key(key, ind, [ind]))
+                except Exception as err:
+                    raise ParseError(
+                        f'Could not parse keycode "{key}" in layer "{layer_name}" with exception "{err}"'
+                    ) from err
 
         layers = self.add_held_keys(layers)
         keymap_data = KeymapData(layers=layers, layout=None, config=None)

keymap_drawer/parse/parse.py
@@ -11,6 +11,10 @@ from keymap_drawer.config import ParseConfig
 from keymap_drawer.keymap import KeymapData, LayoutKey
 
 
+class ParseError(Exception):
+    """Error type for exceptions that happen during keymap parsing."""
+
+
 class KeymapParser(ABC):  # pylint: disable=too-many-instance-attributes
     """Abstract base class for parsing firmware keymap representations."""
 

keymap_drawer/parse/zmk.py
@@ -10,7 +10,7 @@ import yaml
 from keymap_drawer.config import ParseConfig
 from keymap_drawer.keymap import ComboSpec, KeymapData, LayoutKey
 from keymap_drawer.parse.dts import DeviceTree
-from keymap_drawer.parse.parse import KeymapParser
+from keymap_drawer.parse.parse import KeymapParser, ParseError
 
 ZMK_LAYOUTS_PATH = Path(__file__).parent.parent.parent / "resources" / "zmk_keyboard_layouts.yaml"
 
@@ -119,9 +119,9 @@ class ZmkKeymapParser(KeymapParser):
             out = {}
             for node in dts.get_compatible_nodes(compatible_value):
                 if not (bindings := node.get_phandle_array("(?<!sensor-)bindings")):
-                    raise RuntimeError(f'Cannot parse bindings for behavior "{node.name}"')
+                    raise ParseError(f'Cannot parse bindings for behavior "{node.name}"')
                 if node.label is None:
-                    raise RuntimeError(f'Cannot find label for behavior "{node.name}"')
+                    raise ParseError(f'Cannot find label for behavior "{node.name}"')
                 out[f"&{node.label}"] = bindings[:n_bindings]
             return out
 
@@ -134,14 +134,14 @@ class ZmkKeymapParser(KeymapParser):
         cl_nodes = [node for parent in cl_parents for node in parent.children]
         for node in cl_nodes:
             if not (then_layer_val := node.get_array("then-layer")):
-                raise RuntimeError(f'Could not parse `then-layer` for conditional layer node "{node.name}"')
+                raise ParseError(f'Could not parse `then-layer` for conditional layer node "{node.name}"')
             if not (if_layers := node.get_array("if-layers")):
-                raise RuntimeError(f'Could not parse `if-layers` for conditional layer node "{node.name}"')
+                raise ParseError(f'Could not parse `if-layers` for conditional layer node "{node.name}"')
             self.conditional_layers[int(then_layer_val[0])] = [int(val) for val in if_layers]
 
     def _get_layers(self, dts: DeviceTree) -> dict[str, list[LayoutKey]]:
         if not (layer_parents := dts.get_compatible_nodes("zmk,keymap")):
-            raise RuntimeError('Could not find any keymap nodes with "zmk,keymap" compatible property')
+            raise ParseError('Could not find any keymap nodes with "zmk,keymap" compatible property')
 
         layer_nodes = [node for parent in layer_parents for node in parent.children]
         if self.layer_names is None:
@@ -154,14 +154,20 @@ class ZmkKeymapParser(KeymapParser):
                 l_p := len(layer_nodes)
             ), f"Length of provided layer name list ({l_u}) does not match the number of parsed layers ({l_p})"
 
-        layers = {}
+        layers: dict[str, list[LayoutKey]] = {}
         for layer_ind, node in enumerate(layer_nodes):
-            if bindings := node.get_phandle_array(r"(?<!sensor-)bindings"):
-                layers[self.layer_names[layer_ind]] = [
-                    self._str_to_key(binding, layer_ind, [i]) for i, binding in enumerate(bindings)
-                ]
+            layer_name = self.layer_names[layer_ind]
+            if bindings := node.get_phandle_array(r"(?<!-)bindings"):
+                layers[layer_name] = []
+                for ind, binding in enumerate(bindings):
+                    try:
+                        layers[layer_name].append(self._str_to_key(binding, layer_ind, [ind]))
+                    except Exception as err:
+                        raise ParseError(
+                            f'Could not parse binding "{binding}" in layer "{layer_name}" with exception "{err}"'
+                        ) from err
             else:
-                raise RuntimeError(f'Could not parse `bindings` property under layer node "{node.name}"')
+                raise ParseError(f'Could not parse `bindings` property under layer node "{node.name}"')
         return layers
 
     def _get_combos(self, dts: DeviceTree) -> list[ComboSpec]:
@@ -173,15 +179,21 @@ class ZmkKeymapParser(KeymapParser):
         combos = []
         for node in combo_nodes:
             if not (bindings := node.get_phandle_array("bindings")):
-                raise RuntimeError(f'Could not parse `bindings` for combo node "{node.name}"')
+                raise ParseError(f'Could not parse `bindings` for combo node "{node.name}"')
             if not (key_pos_strs := node.get_array("key-positions")):
-                raise RuntimeError(f'Could not parse `key-positions` for combo node "{node.name}"')
+                raise ParseError(f'Could not parse `key-positions` for combo node "{node.name}"')
 
             key_pos = [int(pos) for pos in key_pos_strs]
-            combo = {
-                "k": self._str_to_key(bindings[0], None, key_pos, no_shifted=True),  # ignore current layer for combos
-                "p": key_pos,
-            }
+
+            try:
+                # ignore current layer for combos
+                parsed_key = self._str_to_key(bindings[0], None, key_pos, no_shifted=True)
+            except Exception as err:
+                raise ParseError(
+                    f'Could not parse binding "{bindings[0]}" in combo node "{node.name}" with exception "{err}"'
+                ) from err
+
+            combo = {"k": parsed_key, "p": key_pos}
             if layers := node.get_array("layers"):
                 combo["l"] = [self.layer_names[int(layer)] for layer in layers]
 

commit 10a41cfab490625805eb02577eb79950b9daaa3c

fix: Fix QMK held key inference bug caused by e9efae19

keymap_drawer/parse/qmk.py
@@ -109,7 +109,7 @@ class QmkJsonParser(KeymapParser):
             layers[layer_name] = []
             for ind, key in enumerate(layer):
                 try:
-                    layers[layer_name].append(self._str_to_key(key, ind, [ind]))
+                    layers[layer_name].append(self._str_to_key(key, layer_ind, [ind]))
                 except Exception as err:
                     raise ParseError(
                         f'Could not parse keycode "{key}" in layer "{layer_name}" with exception "{err}"'