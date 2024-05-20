9ea64e8f07ead748f893d56591a15da8c5934f15
Cem Aksoylar <caksoylar@users.noreply.github.com>
2024-05-19 22:36:07 -0700

    Rough parsing done, no combos

--- keymap_drawer/parse/kanata.py
+++ keymap_drawer/parse/kanata.py
@@ -13,15 +13,18 @@ from keymap_drawer.keymap import ComboSpec, KeymapData, LayoutKey
 from keymap_drawer.parse.parse import KeymapParser, ParseError
 
 
-_DEFSRC_60 = [  # fmt: skip
+# fmt: off
+_DEFSRC_60 = [
     "grv", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "bspc",
     "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
     "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "ret",
     "lsft", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "rsft",
     "lctl", "lmet", "lalt", "spc", "ralt", "rmet", "cmps", "rctl"
 ]
+# fmt: on
 DEFSRC_TO_POS = {key: pos for pos, key in enumerate(_DEFSRC_60)}
 PHYSICAL_LAYOUT = {"qmk_keyboard": "1upkeyboards/1up60rgb", "qmk_layout": "LAYOUT_60_ansi"}
+
 
 class KanataKeymapParser(KeymapParser):
     """Parser for Kanata cfg keymaps, using pyparsing-based parsers."""
@@ -38,20 +41,79 @@ class KanataKeymapParser(KeymapParser):
         super().__init__(config, columns, base_keymap, layer_names)
         self.aliases = {}
 
-    def _parse_cfg(self, cfg_str: str) -> list[str | pp.ParseResults]:
-        return (pp.nested_expr("(", ")")[1, ...]
+    @classmethod
+    def _parse_cfg(cls, cfg_str: str, file_path: Path | None) -> list[str | pp.ParseResults]:
+        parsed = (
+            pp.nested_expr("(", ")")
             .ignore(";;" + pp.SkipTo(pp.lineEnd))
             .ignore("#|" + pp.SkipTo("|#"))
-            .parse_string(cfg_str))
+            .parse_string("(" + cfg_str + ")")[0]
+        )
+        if file_path is None:
+            return parsed
+        includes = [node[1] for node in parsed if isinstance(node, pp.ParseResults) and node[0] == "include"]
+        for include in includes[::-1]:
+            with open(file_path.parent / Path(include), encoding="utf-8") as f:
+                parsed = cls._parse_cfg(f.read, None) + parsed
+        return parsed
+
+    @classmethod
+    def _element_to_str(cls, elt: str | pp.ParseResults) -> str:
+        if isinstance(elt, str):
+            return elt
+        return "(" + " ".join(cls._element_to_str(sub) for sub in elt) + ")"
 
     def _str_to_key(  # pylint: disable=too-many-return-statements,too-many-locals
-        self, binding: str, current_layer: int | None, key_positions: Sequence[int], no_shifted: bool = False
+        self, binding: str | pp.ParseResults, current_layer: int | None, key_positions: Sequence[int], no_shifted: bool = False
     ) -> LayoutKey:
-        return LayoutKey(tap=binding)
-    #     if binding in self.raw_binding_map:
-    #         return LayoutKey.from_key_spec(self.raw_binding_map[binding])
-    #     if self.cfg.skip_binding_parsing:
-    #         return LayoutKey(tap=binding)
+        # print(binding)
+        if binding in self.raw_binding_map:
+            return LayoutKey.from_key_spec(self.raw_binding_map[binding])
+        if self.cfg.skip_binding_parsing:
+            return LayoutKey(tap=binding)
+
+        def recurse(new_binding):
+            return self._str_to_key(new_binding, current_layer, key_positions) 
+
+        orig_binding = binding
+
+        if isinstance(binding, str) and binding.startswith("@"):
+            binding = self.aliases.get(binding[1:], binding)
+
+        if isinstance(binding, str):
+            if binding in ("_", "‗", "≝"):
+                return LayoutKey.from_key_spec(self.cfg.trans_legend)
+            if binding in ("XX", "✗", "∅", "•"):
+                return LayoutKey()
+            if binding.startswith("🔣"):
+                return LayoutKey(tap=binding[1:])
+            return LayoutKey(tap=binding)
+
+        assert isinstance(binding, pp.ParseResults)
+
+        if binding[0].startswith("tap-hold"):
+            tap_key = recurse(binding[3])
+            hold_key = recurse(binding[4])
+            return LayoutKey(tap=tap_key.tap, hold=hold_key.tap, shifted=tap_key.shifted)
+        if binding[0] == "layer-switch":
+            return LayoutKey(tap=binding[1], hold=self.cfg.toggle_label)
+        if binding[0] in ("layer-while-held", "layer-toggle"):
+            return LayoutKey(tap=binding[1])
+        if binding[0] in ("unicode", "🔣"):
+            return LayoutKey(tap=binding[1])
+        if binding[0].startswith("release-"):
+            return LayoutKey(tap=binding[1], hold="release")
+        if binding[0].startswith("one-shot"):
+            l_key = recurse(binding[2])
+            return LayoutKey(tap=l_key.tap, hold=self.cfg.sticky_label, shifted=l_key.shifted)
+        if binding[0] == "fork":
+            main_key = recurse(binding[1])
+            alt_key = recurse(binding[2])
+            return LayoutKey(tap=main_key.tap, hold=main_key.hold, shifted=alt_key.tap)
+        if binding[0] == "multi":
+            return LayoutKey(tap=" +".join(recurse(bind).tap for bind in binding[1:]))
+
+        return LayoutKey(tap=self._element_to_str(binding))
 
     #     assert self.layer_names is not None
 
@@ -114,24 +176,6 @@ class KanataKeymapParser(KeymapParser):
     #             return LayoutKey(tap=ref)
     #     return LayoutKey(tap=binding)
 
-    # def _update_behaviors(self, dts: DeviceTree) -> None:
-    #     def get_behavior_bindings(compatible_value: str, n_bindings: int) -> dict[str, list[str]]:
-    #         out = {}
-    #         for node in dts.get_compatible_nodes(compatible_value):
-    #             if not (bindings := node.get_phandle_array("(?<!sensor-)bindings")):
-    #                 raise ParseError(f'Cannot parse bindings for behavior "{node.name}"')
-    #             if node.label is None:
-    #                 raise ParseError(f'Cannot find label for behavior "{node.name}"')
-    #             out[f"&{node.label}"] = bindings[:n_bindings]
-    #         return out
-
-    #     self.hold_taps |= get_behavior_bindings("zmk,behavior-hold-tap", 2)
-    #     self.mod_morphs |= get_behavior_bindings("zmk,behavior-mod-morph", 2)
-    #     self.sticky_keys |= get_behavior_bindings("zmk,behavior-sticky-key", 1)
-
-    # def _get_layers(self, dts: DeviceTree) -> dict[str, list[LayoutKey]]:
-    #     return {}
-
     # def _get_combos(self, dts: DeviceTree) -> list[ComboSpec]:
     #     return []
 
@@ -142,17 +186,14 @@ class KanataKeymapParser(KeymapParser):
         """
         Parse a ZMK keymap with its content and path and return the layout spec and KeymapData to be dumped to YAML.
         """
-        nodes = self._parse_cfg(in_str)
-        # print(nodes, sep="\n")
+        nodes = self._parse_cfg(in_str, Path(file_name))
         defsrc = next(node[1:] for node in nodes if node[0] == "defsrc")
-        # print(defsrc)
 
         try:
-            defalias = next(node[1:] for node in nodes if node[0] == "defalias")
+            defalias = list(chain.from_iterable(node[1:] for node in nodes if node[0] == "defalias"))
             self.aliases = {defalias[k]: defalias[k + 1] for k in range(0, len(defalias), 2)}
         except StopIteration:
             self.aliases = {}
-        # print(self.aliases)
 
         layer_nodes = {node[1]: node[2:] for node in nodes if node[0] == "deflayer"}
         # print(layer_nodes)
