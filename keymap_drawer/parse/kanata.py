7182d90571bcec8e435599140be56a50b81b45dc
Cem Aksoylar <caksoylar@users.noreply.github.com>
2024-05-19 22:46:18 -0700

    Clean up and add layer activations

--- keymap_drawer/parse/kanata.py
+++ keymap_drawer/parse/kanata.py
@@ -5,7 +5,6 @@ from itertools import chain
 from pathlib import Path
 from typing import Sequence
 
-import yaml
 import pyparsing as pp
 
 from keymap_drawer.config import ParseConfig
@@ -63,19 +62,17 @@ class KanataKeymapParser(KeymapParser):
             return elt
         return "(" + " ".join(cls._element_to_str(sub) for sub in elt) + ")"
 
-    def _str_to_key(  # pylint: disable=too-many-return-statements,too-many-locals
+    def _str_to_key(  # pylint: disable=too-many-return-statements,too-many-locals,too-many-branches
         self, binding: str | pp.ParseResults, current_layer: int | None, key_positions: Sequence[int], no_shifted: bool = False
     ) -> LayoutKey:
-        # print(binding)
-        if binding in self.raw_binding_map:
-            return LayoutKey.from_key_spec(self.raw_binding_map[binding])
+        binding_str = self._element_to_str(binding)
+        if binding_str in self.raw_binding_map:
+            return LayoutKey.from_key_spec(self.raw_binding_map[binding_str])
         if self.cfg.skip_binding_parsing:
-            return LayoutKey(tap=binding)
+            return LayoutKey(tap=binding_str)
 
         def recurse(new_binding):
-            return self._str_to_key(new_binding, current_layer, key_positions) 
-
-        orig_binding = binding
+            return self._str_to_key(new_binding, current_layer, key_positions)
 
         if isinstance(binding, str) and binding.startswith("@"):
             binding = self.aliases.get(binding[1:], binding)
@@ -98,6 +95,9 @@ class KanataKeymapParser(KeymapParser):
         if binding[0] == "layer-switch":
             return LayoutKey(tap=binding[1], hold=self.cfg.toggle_label)
         if binding[0] in ("layer-while-held", "layer-toggle"):
+            self.update_layer_activated_from(
+                [current_layer], self.layer_names.index(binding[1]), key_positions
+            )
             return LayoutKey(tap=binding[1])
         if binding[0] in ("unicode", "🔣"):
             return LayoutKey(tap=binding[1])
@@ -113,88 +113,9 @@ class KanataKeymapParser(KeymapParser):
         if binding[0] == "multi":
             return LayoutKey(tap=" +".join(recurse(bind).tap for bind in binding[1:]))
 
-        return LayoutKey(tap=self._element_to_str(binding))
+        return LayoutKey(tap=binding_str)
 
-    #     assert self.layer_names is not None
-
-    #     def mapped(key: str) -> LayoutKey:
-    #         key, mods = self.parse_modifier_fns(key)
-    #         if self._prefix_re is not None:
-    #             key = self._prefix_re.sub("", key)
-    #         mapped = LayoutKey.from_key_spec(
-    #             self.cfg.zmk_keycode_map.get(
-    #                 key,
-    #                 self._numbers_re.sub(r"\3", key)
-    #                 .removeprefix("C_")
-    #                 .removeprefix("K_")
-    #                 .replace("BT_SEL", "BT")
-    #                 .replace("_", " "),
-    #             )
-    #         )
-    #         if no_shifted:
-    #             mapped.shifted = ""
-    #         if mods:
-    #             mapped.apply_formatter(lambda key: self.format_modified_keys(key, mods))
-    #         return mapped
-
-    #     match binding.split():
-    #         case ["&none", *_]:
-    #             return LayoutKey()
-    #         case ["&trans"]:
-    #             return self.trans_key
-    #         case [ref, *_] if ref in self.mod_morphs:
-    #             tap_key = self._str_to_key(self.mod_morphs[ref][0], current_layer, key_positions)
-    #             shifted_key = self._str_to_key(self.mod_morphs[ref][1], current_layer, key_positions)
-    #             return LayoutKey(tap=tap_key.tap, hold=tap_key.hold, shifted=shifted_key.tap)
-    #         case ["&kp", *pars]:
-    #             return mapped(" ".join(pars))
-    #         case ["&kt", *pars]:
-    #             l_key = mapped(" ".join(pars))
-    #             return LayoutKey(tap=l_key.tap, hold=self.cfg.toggle_label, shifted=l_key.shifted)
-    #         case [ref, *pars] if ref in self.sticky_keys:
-    #             l_key = self._str_to_key(f"{self.sticky_keys[ref][0]} {' '.join(pars)}", current_layer, key_positions)
-    #             return LayoutKey(tap=l_key.tap, hold=self.cfg.sticky_label, shifted=l_key.shifted)
-    #         case ["&bt", *pars]:
-    #             mapped_action = mapped(pars[0])
-    #             if len(pars) == 1:
-    #                 return mapped_action
-    #             return LayoutKey(tap=mapped_action.tap, shifted=mapped_action.shifted, hold=pars[1])
-    #         case [("&out" | "&ext_power" | "&rgb_ug"), *pars]:
-    #             return LayoutKey(tap=" ".join(pars).replace("_", " "))
-    #         case [("&mo" | "&to" | "&tog") as behavior, par]:
-    #             if behavior in ("&mo",):
-    #                 self.update_layer_activated_from(
-    #                     [current_layer] if current_layer is not None else [], int(par), key_positions
-    #                 )
-    #                 return LayoutKey(tap=self.layer_names[int(par)])
-    #             return LayoutKey(tap=self.layer_names[int(par)], hold=self.cfg.toggle_label)
-    #         case [ref, hold_par, tap_par] if ref in self.hold_taps:
-    #             hold_key = self._str_to_key(f"{self.hold_taps[ref][0]} {hold_par}", current_layer, key_positions)
-    #             tap_key = self._str_to_key(f"{self.hold_taps[ref][1]} {tap_par}", current_layer, key_positions)
-    #             return LayoutKey(tap=tap_key.tap, hold=hold_key.tap, shifted=tap_key.shifted)
-    #         case [ref] | [ref, "0"]:
-    #             return LayoutKey(tap=ref)
-    #     return LayoutKey(tap=binding)
-
-    # def _get_combos(self, dts: DeviceTree) -> list[ComboSpec]:
-    #     return []
-
-    # def _get_physical_layout(self, file_name: str | None, dts: DeviceTree) -> dict:
-    #     pass
-
-    def _parse(self, in_str: str, file_name: str | None = None) -> tuple[dict, KeymapData]:
-        """
-        Parse a ZMK keymap with its content and path and return the layout spec and KeymapData to be dumped to YAML.
-        """
-        nodes = self._parse_cfg(in_str, Path(file_name))
-        defsrc = next(node[1:] for node in nodes if node[0] == "defsrc")
-
-        try:
-            defalias = list(chain.from_iterable(node[1:] for node in nodes if node[0] == "defalias"))
-            self.aliases = {defalias[k]: defalias[k + 1] for k in range(0, len(defalias), 2)}
-        except StopIteration:
-            self.aliases = {}
-
+    def _get_layers(self, defsrc: list[str], nodes: list[pp.ParseResults]) -> dict[str, list[LayoutKey]]:
         layer_nodes = {node[1]: node[2:] for node in nodes if node[0] == "deflayer"}
         # print(layer_nodes)
         self.layer_names = list(layer_nodes)
@@ -210,7 +131,25 @@ class KanataKeymapParser(KeymapParser):
                         raise ParseError(
                             f'Could not parse keycode "{layer_key}" in layer "{layer_name}" with exception "{err}"'
                         ) from err
+        return layers
 
+    # def _get_combos(self, dts: DeviceTree) -> list[ComboSpec]:
+    #     return []
+
+    def _parse(self, in_str: str, file_name: str | None = None) -> tuple[dict, KeymapData]:
+        """
+        Parse a ZMK keymap with its content and path and return the layout spec and KeymapData to be dumped to YAML.
+        """
+        nodes = self._parse_cfg(in_str, Path(file_name))
+        defsrc = next(node[1:] for node in nodes if node[0] == "defsrc")
+
+        try:
+            defalias = list(chain.from_iterable(node[1:] for node in nodes if node[0] == "defalias"))
+            self.aliases = {defalias[k]: defalias[k + 1] for k in range(0, len(defalias), 2)}
+        except StopIteration:
+            self.aliases = {}
+
+        layers = self._get_layers(defsrc, nodes)
         layers = self.add_held_keys(layers)
         keymap_data = KeymapData(layers=layers, layout=None, config=None)
 
