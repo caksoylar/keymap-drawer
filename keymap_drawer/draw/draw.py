90dfb4867aef10f7eb79348a0a9300c2f9e02aee
Cem Aksoylar <caksoylar@users.noreply.github.com>
2024-11-28 22:31:08 -0800

    fix: Do not move left/right legends with tap

--- keymap_drawer/draw/draw.py
+++ keymap_drawer/draw/draw.py
@@ -107,13 +107,13 @@ class KeymapDrawer(ComboDrawerMixin, UtilsMixin):
             legend_type="shifted",
         )
         self._draw_legend(
-            tap_shift - Point(w / 2 - self.cfg.inner_pad_w - self.cfg.small_pad, 0),
+            Point(-w / 2 + self.cfg.inner_pad_w + self.cfg.small_pad, 0),
             [l_key.left],
             classes=["key", l_key.type],
             legend_type="left",
         )
         self._draw_legend(
-            tap_shift + Point(w / 2 - self.cfg.inner_pad_w - self.cfg.small_pad, 0),
+            Point(w / 2 - self.cfg.inner_pad_w - self.cfg.small_pad, 0),
             [l_key.right],
             classes=["key", l_key.type],
             legend_type="right",
